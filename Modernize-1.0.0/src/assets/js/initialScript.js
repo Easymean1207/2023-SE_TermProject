27142b96e66e3c467f86bca064e1bbf0a974995b
ksj7385 <117991880+ksj7385@users.noreply.github.com>
2023-05-25 21:36:39 +0900

    어드민 게스트

---
+++ Modernize-1.0.0/src/assets/js/initialScript.js
@@ -0,0 +1,9 @@
+document.getElementById("adminBtn").addEventListener("click", function() {
+    window.location.href = "../html/authentication-login.html";
+});
+
+document.getElementById("guestBtn").addEventListener("click", function() {
+    window.location.href = "../html/authentication-login.html";
+});
+
+
